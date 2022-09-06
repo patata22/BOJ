8a7cfcfbfa1fe2fa667836490f9caa33bce9bcb4
patata22 <98090883+patata22@users.noreply.github.com>
2022-09-06 19:47:31 +0900

    [Silver II] Title: 나무 자르기, Time: 536 ms, Memory: 262752 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,19 +1,14 @@
-def total(x):
-    answer=0
-    for tree in trees:
-        if tree>x:answer+=tree-x
-    if answer>=m:return True
-    else:return False
-    
-def binary_search():
-    st,en=0,max(trees)
-    while st<en:
-        mid=(st+en+1)//2
-        if total(mid):st=mid
-        else:en=mid-1
-    return st
-
-
 n,m=map(int,input().split())
-trees=list(map(int,input().split()))
-print(binary_search())
+tree=list(map(int,input().split()))
+l=0
+r=2000000001
+while l+1<r:
+    
+    mid=(l+r)//2
+    temp=0
+    for x in tree:
+        temp+=max(x-mid,0)
+    if temp>=m:
+        l=mid
+    else: r=mid
+print(l)
