107102cee5c0506caf727a0f24c5951ea6ac3a30
patata22 <98090883+patata22@users.noreply.github.com>
2022-11-25 16:39:23 +0900

    [Silver I] Title: 트리 순회, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,29 @@
+def pre(root):
+    if root!='.':
+        print(root,end='')
+        pre(tree[root][0])
+        pre(tree[root][1])
+def mid(root):
+    if root!='.':
+        mid(tree[root][0])
+        print(root,end='')
+        mid(tree[root][1])
+def post(root):
+    if root!='.':
+        post(tree[root][0])
+        post(tree[root][1])
+        print(root,end='')
+        
+
+
+n=int(input())
+tree={}
+for _ in range(n):
+    root,left,right=input().split()
+    tree[root]=[left,right]
+
+pre('A')
+print()
+mid('A')
+print()
+post('A')
