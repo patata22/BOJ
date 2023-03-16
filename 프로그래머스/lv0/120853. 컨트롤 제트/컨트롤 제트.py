bb4dcc3826a020e9d12a4a2cd874458f33075e51
patata22 <98090883+patata22@users.noreply.github.com>
2023-03-17 00:57:19 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -0,0 +1,8 @@
+def solution(s):
+    print(type(s))
+    answer=0
+    s=s.split()
+    for i in range(len(s)):
+        if s[i]=='Z': answer-=int(s[i-1])
+        else: answer+=int(s[i])
+    return answer
