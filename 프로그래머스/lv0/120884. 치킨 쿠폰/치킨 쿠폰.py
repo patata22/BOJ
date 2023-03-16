ad448c8cf93bc0f17fad66506282a43670512ed0
patata22 <98090883+patata22@users.noreply.github.com>
2023-03-17 06:09:05 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.00 ms, Memory: 9.99 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -0,0 +1,7 @@
+def solution(chicken):
+    answer=chicken//10
+    chicken= chicken//10+chicken%10
+    while chicken>=10:
+        answer+=chicken//10
+        chicken = chicken//10+chicken%10
+    return answer
