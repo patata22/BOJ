c744cb6d39a6dadb0ad1b47644720b34bee981f6
patata22 <98090883+patata22@users.noreply.github.com>
2023-04-03 13:16:34 +0900

    [Silver III] Title: 문자열 집합, Time: 4236 ms, Memory: 1609880 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,13 +1,51 @@
 import sys
 input=sys.stdin.readline
 
-dic={}
-n,m=map(int,input().split())
-for _ in range(n):
-    x=input().rstrip()
-    dic[x]=0
+def charToInt(x):
+    return ord(x)-97
+
+def insert(s):
+    global unused
+    now=1
+    for x in s:
+        char=charToInt(x)
+        if nxt[now][char]==-1:
+            nxt[now][char]=unused
+            unused+=1
+        now=nxt[now][char]
+    check[now]+=1
+
+def find(s):
+    now=1
+    for x in s:
+        char=charToInt(x)
+        if nxt[now][char]==-1:
+            return False
+        now=nxt[now][char]
+    return bool(check[now])
+
+def erase(s):
+    now=1
+    for x in s:
+        char=charToInt(x)
+        if nxt[now][char]==-1:
+            return
+        now=nxt[now][char]
+    check[now]-=1
+    
+n,m=map(int,input().split())        
+unused=2
+MAX= n*500+5
+
+nxt=[[-1]*26 for i in range(MAX)]
+check=[0]*MAX
+
 answer=0
+for _ in range(n):
+    insert(input().rstrip())
+
 for _ in range(m):
-    x=input().rstrip()
-    if x in dic:answer+=1
+    answer+=find(input().rstrip())
+
 print(answer)
+    
