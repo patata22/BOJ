fc2e9422b0421dbb48a9ec0f4cd355ea2da5e8a5
patata22 <98090883+patata22@users.noreply.github.com>
2023-04-03 21:50:23 +0900

    [Platinum IV] Title: 열혈강호, Time: 624 ms, Memory: 137720 KB -BaekjoonHub

---
+++ 백준/Platinum/11375. 열혈강호/열혈강호.py
@@ -0,0 +1,19 @@
+import sys
+input=sys.stdin.readline
+
+def dfs(x):
+    if visited[x]: return False
+    visited[x]=1
+    for t in task[x]:
+        if solved[t]==-1 or dfs(solved[t]):
+            solved[t]=x
+            return True
+
+n,m=map(int,input().split())
+task=[list(map(int,input().split()))[1:] for i in range(n)]
+solved= [-1]*(m+1)
+answer=0
+for i in range(n):
+    visited=[0]*n
+    if dfs(i):answer+=1
+print(answer)
