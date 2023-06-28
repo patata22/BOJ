3d44f8e3538ca566d8a2a9ac36c92215f758cf5d
patata22 <98090883+patata22@users.noreply.github.com>
2023-06-28 20:16:39 +0900

    [Gold V] Title: 평범한 배낭, Time: 4524 ms, Memory: 279396 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,10 +1,15 @@
 n,k=map(int,input().split())
-item = [list(map(int,input().split())) for i in range(n)]
-dp=[[0]*(k+1) for i in range(n)]
+item=[(0,0)]
+for _ in range(n):
+    item.append(tuple(map(int,input().split())))
 
-for i in range(n):
-    for w in range(k+1):
-        w_i, v_i = item[i]
-        if w_i>w: dp[i][w]=dp[i-1][w]
-        else: dp[i][w]= max(v_i + dp[i-1][w-w_i], dp[i-1][w])
+dp=[[0]*(k+1) for i in range(n+1)]
+
+for i in range(1,n+1):
+    w,v=item[i]
+    for j in range(k+1):
+        if j<w:
+            dp[i][j]=dp[i-1][j]
+        else:
+            dp[i][j]=max(dp[i-1][j-w]+v,dp[i-1][j])
 print(dp[-1][-1])
