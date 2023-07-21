8cb1bc0a52dc1e1a184956be894c31bf75698797
patata22 <98090883+patata22@users.noreply.github.com>
2023-07-21 15:34:37 +0900

    [Gold I] Title: 합성함수와 쿼리, Time: 4268 ms, Memory: 101992 KB -BaekjoonHub

--- 백준/Gold/17435. 합성함수와 쿼리/합성함수와 쿼리.py
+++ 백준/Gold/17435. 합성함수와 쿼리/합성함수와 쿼리.py
@@ -1,23 +1,23 @@
 import sys
 input=sys.stdin.readline
 
-MAX=500001
-MAX_D=19
+MAX=19
 
 m=int(input())
-table = [[0]*(MAX_D+1) for i in range(m+1)]
 
+table= [[0]*20 for i in range(m+1)]
 
-temp = tuple(map(int,input().split()))
+temp=tuple(map(int,input().split()))
 for i in range(1,m+1):
-    table[i][0] = temp[i-1]
+    table[i][0]=temp[i-1]
 
-for j in range(1,MAX_D+1):
+for j in range(1,20):
     for i in range(1,m+1):
         table[i][j] = table[table[i][j-1]][j-1]
 
 for _ in range(int(input())):
     n,x=map(int,input().split())
-    for i in range(MAX_D+1):
-        if (n>>i)&1==1: x = table[x][i]
+    for i in range(20):
+        if n&(1<<i): x = table[x][i]
     print(x)
+    
