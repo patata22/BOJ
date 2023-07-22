2d8e947ad5278fed50892b52d7dd070b12095218
patata22 <98090883+patata22@users.noreply.github.com>
2023-07-22 20:38:07 +0900

    [Platinum V] Title: Strongly Connected Component, Time: 184 ms, Memory: 36804 KB -BaekjoonHub

--- 백준/Platinum/2150. Strongly Connected Component/Strongly Connected Component.py
+++ 백준/Platinum/2150. Strongly Connected Component/Strongly Connected Component.py
@@ -1,48 +1,52 @@
 import sys
 input=sys.stdin.readline
-sys.setrecursionlimit(10**5)
+sys.setrecursionlimit(10**6)
 
 def dfs(now):
-    global cnt,sccCnt
+    global cnt, scc_cnt
     cnt+=1
-    dfsn[now]=cnt
+    visited[now]=cnt
     stack.append(now)
 
-    result=cnt
+    result=visited[now]
     for to in graph[now]:
-        if not dfsn[to]:
-            result = min(result,dfs(to))
-        elif not finished[to]: result = min(result, dfsn[to])
+        if not visited[to]: result=min(result, dfs(to))
+        elif not finished[to]: result = min(result, visited[to])
 
-    if result==dfsn[now]:
+    if result==visited[now]:
         scc=[]
+        scc_cnt+=1
         while True:
             t=stack.pop()
             scc.append(t)
             finished[t]=1
-            sccNum[t]=sccCnt
             if t==now: break
+
         scc.sort()
         scc.append(-1)
         SCC.append(scc)
-        sccCnt+=1
+
     return result
-    
+            
 v,e=map(int,input().split())
-cnt=0
-sccCnt=0
-graph= [[] for i in range(v+1)]
+graph=[[] for i in range(v+1)]
 for _ in range(e):
     a,b=map(int,input().split())
     graph[a].append(b)
-dfsn=[0]*(v+1)
-sccNum=[-1]*(v+1)
+    
+visited=[0]*(v+1)
 finished=[0]*(v+1)
-stack=[]
+cnt=0
+scc_cnt=0
+
+
 SCC=[]
+stack=[]
 
 for i in range(1,v+1):
-    if not dfsn[i]: dfs(i)
+    if not visited[i]:
+        dfs(i)
+
+print(scc_cnt)
 SCC.sort()
-print(sccCnt)
 for scc in SCC: print(*scc)
