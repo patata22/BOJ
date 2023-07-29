2eb93c6737c512aeb7346502ccf8d1c6cf0928d6
patata22 <98090883+patata22@users.noreply.github.com>
2023-07-29 18:44:30 +0900

    [Platinum IV] Title: 광고, Time: 316 ms, Memory: 71792 KB -BaekjoonHub

---
+++ 백준/Platinum/1305. 광고/광고.py
@@ -0,0 +1,14 @@
+def failure(s):
+    l=len(s)
+    fail = [0]*l
+    j=0
+    for i in range(1,l):
+        while j>0 and s[i]!=s[j]: j=fail[j-1]
+        if s[i]==s[j]:
+            j+=1
+            fail[i]=j
+    return fail
+
+n=int(input())
+fail = failure(input())
+print(n-fail[-1])
