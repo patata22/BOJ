ff519c8999504519f9298c7429fa0a3a11332131
patata22 <98090883+patata22@users.noreply.github.com>
2023-09-03 19:41:00 +0900

    [Bronze II] Title: 나는 친구가 적다 (Large), Time: 80 ms, Memory: 33036 KB -BaekjoonHub

--- 백준/Bronze/16172. 나는 친구가 적다 （Large）/나는 친구가 적다 （Large）.py
+++ 백준/Bronze/16172. 나는 친구가 적다 （Large）/나는 친구가 적다 （Large）.py
@@ -1,9 +1,6 @@
-temp=list(input())
-origin=""
-for x in temp:
+temp=[]
+for x in input():
     if 48<=ord(x)<=57: continue
-    origin+=x
-
-target=input()
-if target in origin:print(1)
-else:print(0)
+    temp.append(x)
+temp=''.join(temp)
+print(1) if input() in temp else print(0)
