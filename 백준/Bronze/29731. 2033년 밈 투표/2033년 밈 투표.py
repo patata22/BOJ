e4be2a7d1f46cb6cce2e2a848fa38d40cbf400b4
patata22 <98090883+patata22@users.noreply.github.com>
2023-09-15 14:11:23 +0900

    [Bronze V] Title: 2033년 밈 투표, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/29731. 2033년 밈 투표/2033년 밈 투표.py
@@ -0,0 +1,12 @@
+answer='No'
+x=['Never gonna give you up',
+'Never gonna let you down',
+'Never gonna run around and desert you',
+'Never gonna make you cry',
+'Never gonna say goodbye',
+'Never gonna tell a lie and hurt you',
+'Never gonna stop']
+for _ in range(int(input())):
+    if input() not in x:
+        answer='Yes'
+print(answer)
