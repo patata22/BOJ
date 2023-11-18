1e9130dad60e9f6992c24adc65728d4a29150208
patata22 <98090883+patata22@users.noreply.github.com>
2023-11-18 21:07:28 +0900

    [Bronze III] Title: 아이그루스와 화장실, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30642. 아이그루스와 화장실/아이그루스와 화장실.py
@@ -0,0 +1,7 @@
+n=int(input())
+if input()=='annyong':
+    temp=int(input())//2*2+1
+    print(temp) if temp<=n else print(temp-2)
+else:
+    temp=int(input())//2*2
+    print(temp) if temp else print(temp+2)
