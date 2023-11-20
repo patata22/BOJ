2bdd4a347d720e55469d8c2d9c196056f22723e7
patata22 <98090883+patata22@users.noreply.github.com>
2023-11-20 18:19:01 +0900

    [Bronze III] Title: Secret Location, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5340. Secret Location/Secret Location.py
@@ -0,0 +1,2 @@
+print(f'Latitude {len(input().rstrip())}:{len(input().rstrip())}:{len(input().rstrip())}')
+print(f'Longitude {len(input().rstrip())}:{len(input().rstrip())}:{len(input().rstrip())}')
