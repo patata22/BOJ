a38303a571fe22b1150bee6f13ca37e683be056b
patata22 <98090883+patata22@users.noreply.github.com>
2023-12-22 16:06:36 +0900

    [Silver II] Title: 회의실 배정 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/19621. 회의실 배정 2/회의실 배정 2.py
@@ -0,0 +1,8 @@
+n=int(input())
+task=[tuple(map(int,input().split())) for i in range(n)]
+dp=[[0]*2 for i in range(n)]
+
+for i in range(n):
+    dp[i][0]=max(dp[i-1])
+    dp[i][1]=dp[i-1][0]+task[i][2]
+print(max(dp[-1]))
