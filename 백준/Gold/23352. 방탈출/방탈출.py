ed61216fc64aa25507ffd3abf565c2fa4377be22
patata22 <98090883+patata22@users.noreply.github.com>
2024-01-22 13:21:06 +0900

    [Gold V] Title: 방탈출, Time: 4500 ms, Memory: 34104 KB -BaekjoonHub

---
+++ 백준/Gold/23352. 방탈출/방탈출.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+dx=(-1,1,0,0)
+dy=(0,0,-1,1)
+
+def sol(X,Y):
+    global long,answer
+    q=deque()
+    q.append((X,Y))
+    before=deque()   
+    visited=[[0]*m for i in range(n)]
+    visited[X][Y]=1
+    t=1
+    while q:
+        for _ in range(len(q)):
+            x,y=q.popleft()
+            before.append((x,y))
+            for i in range(4):
+                nx,ny=x+dx[i],y+dy[i]
+                if 0<=nx<n and 0<=ny<m and board[nx][ny] and not visited[nx][ny]:
+                    visited[nx][ny]=1
+                    q.append((nx,ny))
+        if q:
+            before=deque()
+            t+=1
+        elif t>=long:
+            temp=0
+            for x,y in before:
+                temp=max(temp, board[X][Y]+board[x][y])
+            if t>long:
+                long=t
+                answer=temp
+            elif t==long:
+                answer=max(answer,temp)
+                
+n,m=map(int,input().split())
+board=[list(map(int,input().split())) for i in range(n)]
+answer=0
+long=0
+for i in range(n):
+    for j in range(m):
+        if board[i][j]:
+            sol(i,j)
+print(answer)
