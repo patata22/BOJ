b33b2901d03901323d06d0499f6d2e577db5989f
patata22 <98090883+patata22@users.noreply.github.com>
2024-01-29 22:16:13 +0900

    [Gold III] Title: 텀 프로젝트, Time: 2232 ms, Memory: 49396 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -1,25 +1,25 @@
-import sys
-sys.setrecursionlimit(10000000)
-def dfs(i):
-    global total
-    visited[i]=1
-    chain.append(i)
-    n=choice[i]
-    if visited[n]==1:
-        if n in chain:total+=len(chain)-chain.index(n)
-        
-    else:
-        dfs(n)
+from collections import deque
 
+def sol():
+    q=deque()
+    answer=0
+    for i in range(n):
+        if indegree[i]==0:
+            q.append(i)
+            answer+=1
+    while q:
+        now=q.popleft()
+        indegree[nxt[now]]-=1
+        if indegree[nxt[now]]==0:
+            q.append((nxt[now]))
+            answer+=1
+    return answer
 
 for _ in range(int(input())):
     n=int(input())
-    choice=[0] + list(map(int,input().split()))
-    visited=[0 for i in range(n+1)]
-    total=0
+    nxt=list(map(lambda x: int(x)-1, input().split()))
+    indegree=[0]*n
+    for i in range(n):
+        indegree[nxt[i]]+=1
+    print(sol())
 
-    for i in range(1,n+1):
-        if visited[i]==0:
-            chain=[]
-            dfs(i)
-    print(n-total)
