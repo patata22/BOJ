0e8cb5e5e2624cd31e2f9a799c3063057dd2dde6
patata22 <98090883+patata22@users.noreply.github.com>
2024-02-18 07:50:39 +0900

    [Gold V] Title: 화학 실험, Time: 460 ms, Memory: 78376 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/20311. 화학 실험/화학 실험.py
+++ 백준/Gold/20311. 화학 실험/화학 실험.py
@@ -1,20 +1,23 @@
 n,k=map(int,input().split())
-number=list(map(int,input().split()))
+number=sorted(list(enumerate(map(int,input().split()))),key=lambda x: -x[1])
+m=0
+for x,y in number:
+    m=max(m,y)
 answer=[0]*n
-m=max(number)
 if (n%2==0 and m>n//2) or (n%2 and m>n//2+1):print(-1)
 else:
     idx=0
+    count=0
     for i in range(0,n,2):
-        if not number[idx]:
+        if count==number[idx][1]:
             idx+=1
-        answer[i]=idx+1
-        number[idx]-=1
+            count=0
+        answer[i]=number[idx][0]+1
+        count+=1
     for i in range(1,n,2):
-        if not number[idx]:
+        if count==number[idx][1]:
             idx+=1
-        answer[i]=idx+1
-        number[idx]-=1
+            count=0
+        answer[i]=number[idx][0]+1
+        count+=1
     print(*answer)
-        
-                
