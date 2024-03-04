32e2be39a7ea41954b87032e32807bf92c0e26c5
patata22 <98090883+patata22@users.noreply.github.com>
2024-03-04 14:55:02 +0900

    [Platinum IV] Title: 열혈강호, Time: 612 ms, Memory: 126980 KB -BaekjoonHub

--- 백준/Platinum/11375. 열혈강호/열혈강호.py
+++ 백준/Platinum/11375. 열혈강호/열혈강호.py
@@ -1,19 +1,29 @@
 import sys
 input=sys.stdin.readline
+sys.setrecursionlimit(2000)
 
-def dfs(x):
-    if visited[x]: return False
-    visited[x]=1
-    for t in task[x]:
-        if solved[t]==-1 or dfs(solved[t]):
-            solved[t]=x
-            return True
+def dfs(now):
+    visited[now]=1
+    for to in graph[now]:
+        if work[to]==0 or (not visited[work[to]] and dfs(work[to])):
+            worker[now]=to
+            work[to]=now
+            return 1
+    return 0
+
 
 n,m=map(int,input().split())
-task=[list(map(int,input().split()))[1:] for i in range(n)]
-solved= [-1]*(m+1)
+graph=[[] for i in range(n+1)]
+
+for i in range(1,n+1):
+    for x in list(map(int,input().split()))[1:]:
+        graph[i].append(x)
+
+worker=[0]*(n+1)
+work=[0]*(m+1)
 answer=0
-for i in range(n):
-    visited=[0]*n
-    if dfs(i):answer+=1
+
+for i in range(1,n+1):
+    visited=[0]*(n+1)
+    answer+=dfs(i)
 print(answer)
