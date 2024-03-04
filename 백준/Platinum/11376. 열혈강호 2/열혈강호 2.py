2f700a45a3ccf4f99d79d6bb232736206993640e
patata22 <98090883+patata22@users.noreply.github.com>
2024-03-04 15:26:38 +0900

    [Platinum IV] Title: 열혈강호 2, Time: 2276 ms, Memory: 153940 KB -BaekjoonHub

---
+++ 백준/Platinum/11376. 열혈강호 2/열혈강호 2.py
@@ -0,0 +1,30 @@
+import sys
+input=sys.stdin.readline
+
+def dfs(now):
+    visited[now]=1
+    for to in graph[now]:
+        if not work[to] or (not visited[work[to]] and dfs(work[to])):
+            work[to]=now
+            worker[now]=to
+            return 1
+    return 0
+
+
+n,m=map(int,input().split())
+graph=[[] for i in range(2*n+2)]
+worker=[0]*(2*n+2)
+work=[0]*(m+1)
+
+for i in range(1,n+1):
+    for x in list(map(int,input().split()))[1:]:
+        graph[2*i].append(x)
+        graph[2*i+1].append(x)
+answer=0
+
+for i in range(1,n+1):
+    visited=[0]*(2*n+2)
+    answer+=dfs(2*i)
+    visited=[0]*(2*n+2)
+    answer+=dfs(2*i+1)
+print(answer)
