9c04e93e453e62aef7a8eef09415efc370f76f0c
patata22 <98090883+patata22@users.noreply.github.com>
2024-03-05 14:49:41 +0900

    [Gold V] Title: 소셜 네트워킹 어플리케이션, Time: 948 ms, Memory: 31900 KB -BaekjoonHub

--- 백준/Gold/7511. 소셜 네트워킹 어플리케이션/소셜 네트워킹 어플리케이션.py
+++ 백준/Gold/7511. 소셜 네트워킹 어플리케이션/소셜 네트워킹 어플리케이션.py
@@ -7,12 +7,13 @@ def find(a):
     return p[a]
 
 def union(a,b):
-    a,b=find(a),find(b)
-    if a!=b:p[b]=a
-        
-for tt in range(int(input())):
-    if tt:print()
-    print(f'Scenario {tt+1}:')
+    pa=find(a)
+    pb=find(b)
+    if pa!=pb: p[pb]=pa
+
+for t in range(int(input())):
+    if t:print()
+    print(f'Scenario {t+1}:')
     n=int(input())
     p=[-1]*n
     for _ in range(int(input())):
@@ -20,4 +21,5 @@ for tt in range(int(input())):
         union(a,b)
     for _ in range(int(input())):
         a,b=map(int,input().split())
-        print(int(find(a)==find(b)))
+        if find(a)==find(b):print(1)
+        else: print(0)
