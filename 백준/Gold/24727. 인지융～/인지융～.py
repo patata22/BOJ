dc71875640a06905cc9aedb12d12cd24d4434166
patata22 <98090883+patata22@users.noreply.github.com>
2024-04-28 20:26:00 +0900

    [Gold III] Title: 인지융~, Time: 76 ms, Memory: 36388 KB, Score: 78 point -BaekjoonHub

--- 백준/Gold/24727. 인지융～/인지융～.py
+++ 백준/Gold/24727. 인지융～/인지융～.py
@@ -1,10 +1,23 @@
+import heapq
 from collections import deque
 
 dx=(-1,1,0,0)
 dy=(0,0,-1,1)
 
-
-
+def fillA():
+    row=0
+    cnt1=0
+    while cnt1<a and row<n:
+        x=row
+        y=0
+        while 0<=x<n and 0<=y<n:
+            cnt1+=1
+            board[x][y]=1
+            if cnt1==a: return cnt1
+            x-=1
+            y+=1
+        row+=1
+    return cnt1
 def cut():
     q=deque()
     q.append((0,0))
@@ -16,14 +29,18 @@ def cut():
             if 0<=nx<n and 0<=ny<n and not visited[nx][ny]:
                 visited[nx][ny]=1
                 if board[nx][ny]==1: q.append((nx,ny))
-                
-def fill():
-    if visited[-1][-1]:return 0
+    
+def fillB():
     q=deque()
-    cnt2=0
-    q.append((n-1,n-1))
-    board[n-1][n-1]=2
-    visited[n-1][n-1]=1
+    for i in range(n-1,-1,-1):
+        if q: break
+        for j in range(n-1,-1,-1):
+            if not visited[i][j]:
+                board[i][j]=2
+                visited[i][j]=1
+                q.append((i,j))
+                break
+    if not q: return 0
     cnt2=1
     while q:
         x,y=q.popleft()
@@ -31,30 +48,23 @@ def fill():
             nx,ny=x+dx[i],y+dy[i]
             if 0<=nx<n and 0<=ny<n and not visited[nx][ny]:
                 visited[nx][ny]=1
-                q.append((nx,ny))
                 board[nx][ny]=2
                 cnt2+=1
+                q.append((nx,ny))
                 if cnt2==b: return cnt2
     return cnt2
+                
+    
 n=int(input())
 a,b=map(int,input().split())
 board=[[0]*n for i in range(n)]
 visited=[[0]*n for i in range(n)]
-cnt1=0
-for i in range(n):
-    if cnt1==a: break
-    for j in range(n):
-        board[i][j]=1
-        cnt1+=1
-        if cnt1==a: break
 
+cnt1=fillA()
 cut()
-cnt2=fill()
-
-
+cnt2=fillB()
 if cnt1==a and cnt2==b:
     print(1)
     for i in range(n):
         print(*board[i],sep='')
-else:
-    print(-1)
+else: print(-1)
