fd43ad8cc09da4bbdbf2f9a897c6092f982ec090
patata22 <98090883+patata22@users.noreply.github.com>
2024-05-12 16:56:37 +0900

    [Gold V] Title: RUN Number, Time: 56 ms, Memory: 31120 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Gold/31813. RUN Number/RUN Number.py
@@ -0,0 +1,22 @@
+def makeNum(i,n):
+    return int(str(i)*n)
+
+for tt in range(int(input())):
+    n,k=map(int,input().split())
+    answer=[]
+    for i in range(n):
+        now=k//(10**(n-i-1))
+        temp=makeNum(now,n-i)
+        if temp<=k:
+            if temp: answer.append(temp)
+            k-=temp
+        else:
+            temp=makeNum(now-1,n-i)
+            k-=temp
+            if temp: answer.append(temp)
+            
+        if not k: break
+    print(len(answer))
+    print(*answer)
+    
+    
