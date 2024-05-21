f74c07a47c3757d91375fe6ad94fc05f1c66d5c8
patata22 <98090883+patata22@users.noreply.github.com>
2024-05-21 14:36:01 +0900

    [Gold IV] Title: 사탕 줍기 대회, Time: 476 ms, Memory: 46028 KB -BaekjoonHub

---
+++ 백준/Gold/5721. 사탕 줍기 대회/사탕 줍기 대회.py
@@ -0,0 +1,19 @@
+import sys
+input=sys.stdin.readline
+
+def sol(lst):
+    N=len(lst)
+    dp=[[0]*2 for i in range(N)]
+    dp[0][1]=lst[0]
+    for i in range(1,N):
+        dp[i][0]=max(dp[i-1])
+        dp[i][1]=dp[i-1][0]+lst[i]
+    return max(dp[-1])
+    
+
+while True:
+    n,m=map(int,input().split())
+    if n==0: break
+    score=[sol(list(map(int,input().split()))) for i in range(n)]
+    print(sol(score))
+    
