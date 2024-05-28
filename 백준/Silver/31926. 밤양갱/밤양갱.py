d4f1b3fbb05ee4375dee3dfcb4f2ef6a66445e34
patata22 <98090883+patata22@users.noreply.github.com>
2024-05-28 11:56:48 +0900

    [Silver I] Title: 밤양갱, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/31926. 밤양갱/밤양갱.py
@@ -0,0 +1,16 @@
+
+
+n=int(input())
+if n==1: print(10)
+elif n==2: print(11)
+else:
+    answer=9
+    now=2
+    n+=1
+    while now<n:
+        now*=2
+        answer+=1
+    print(answer+1)
+
+
+#마지막에 daldidalgodaldida 복사
