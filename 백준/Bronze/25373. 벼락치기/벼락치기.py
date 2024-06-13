d613a21a95683010db4d1641c518561fb18aaa28
patata22 <98090883+patata22@users.noreply.github.com>
2024-06-13 16:32:26 +0900

    [Bronze II] Title: 벼락치기, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/25373. 벼락치기/벼락치기.py
@@ -0,0 +1,10 @@
+n=int(input())
+if n<=1: print(1)
+elif n<=3:print(2)
+elif n<=6:print(3)
+elif n<=10:print(4)
+elif n<=15:print(5)
+elif n<=21:print(6)
+else:
+    if n%7: print(n//7+4)
+    else: print(n//7+3)
