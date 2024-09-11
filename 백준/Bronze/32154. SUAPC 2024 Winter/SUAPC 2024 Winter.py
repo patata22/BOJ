04d4071f16feb45db51197a940a5f2dea70e17de
patata22 <98090883+patata22@users.noreply.github.com>
2024-09-11 16:40:12 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.py
@@ -0,0 +1,5 @@
+score=[0,11,9,9,9,8,8,8,8,8,8]
+problem=['','ABCDEFGHJLM', 'ACEFGHILM', 'ACEFGHILM', 'ABCEFGHLM','ACEFGHLM', 'ACEFGHLM','ACEFGHLM','ACEFGHLM','ACEFGHLM','ABCFGHLM']
+n=int(input())
+print(score[n])
+print(*list(problem[n]))
