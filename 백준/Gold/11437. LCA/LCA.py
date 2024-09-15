0ae13ce23f7645b1c6a52e8397eb47edf47adad3
patata22 <98090883+patata22@users.noreply.github.com>
2024-09-15 19:59:20 +0900

    [Gold III] Title: LCA, Time: 1572 ms, Memory: 122164 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.py
+++ 백준/Gold/11437. LCA/LCA.py
@@ -1,39 +1,48 @@
+from collections import deque
 import sys
 input=sys.stdin.readline
-sys.setrecursionlimit(10**5)
 
-
-def makeTree(now,dep,parent):
-    p[now]=parent
-    depth[now]=dep
-    for to in graph[now]:
-        if depth[to]==-1:
-            makeTree(to,dep+1,now)
-
-def lca(a,b):
+def makeTree():
+    q=deque()
+    q.append(1)
+    visited[1]=1
+    depth[1]=0
+    dep=1
+    while q:
+        for _ in range(len(q)):
+            now=q.popleft()
+            for to in graph[now]:
+                if not visited[to]:
+                    visited[to]=1
+                    parent[to]=now
+                    depth[to]=dep
+                    q.append(to)
+        dep+=1
+    
+def sol(a,b):
     if depth[a]<depth[b]:
         a,b=b,a
-    diff=depth[a]-depth[b]
-
-    for i in range(diff):
-        a=p[a]
-    while p[a]!=p[b]:
-        a=p[a]
-        b=p[b]
-    if a==b:return a
-    return p[a]
+    while depth[a]>depth[b]:
+        a=parent[a]
+    if a==b: return b
+    while parent[a]!=parent[b]:
+        a=parent[a]
+        b=parent[b]
+    return parent[a]
 
 n=int(input())
 graph=[[] for i in range(n+1)]
-for i in range(n-1):
+for _ in range(n-1):
     a,b=map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-p=[-1]*(n+1)
+visited=[0]*(n+1)
+parent=[-1]*(n+1)
 depth=[-1]*(n+1)
 
-makeTree(1,0,-1)
+makeTree()
+
 for _ in range(int(input())):
     a,b=map(int,input().split())
-    print(lca(a,b))
+    print(sol(a,b))
