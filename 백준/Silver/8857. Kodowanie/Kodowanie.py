28a766f3b68e1fc12aea6d4f823b1387872e571a
patata22 <98090883+patata22@users.noreply.github.com>
2024-09-16 17:24:48 +0900

    [Silver III] Title: Kodowanie, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/8857. Kodowanie/Kodowanie.py
@@ -0,0 +1,12 @@
+import sys
+input=sys.stdin.readline
+
+for tt in range(int(input())):
+    n=int(input())
+    dp=[[0]*2 for i in range(n)]
+    dp[0][1]=1
+    for i in range(1,n):
+        dp[i][0]+=sum(dp[i-1])
+        dp[i][1]+=dp[i-1][0]
+    print(sum(dp[-1]))
+    
