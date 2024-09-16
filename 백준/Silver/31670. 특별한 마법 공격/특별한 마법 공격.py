f0861ca6bc8dc4c4588068b508a4daaed1fca26a
patata22 <98090883+patata22@users.noreply.github.com>
2024-09-16 18:53:40 +0900

    [Silver II] Title: 특별한 마법 공격, Time: 132 ms, Memory: 48180 KB -BaekjoonHub

--- 백준/Silver/31670. 특별한 마법 공격/특별한 마법 공격.py
+++ 백준/Silver/31670. 특별한 마법 공격/특별한 마법 공격.py
@@ -1,11 +1,10 @@
 n=int(input())
 number=list(map(int,input().split()))
-if n==1: print(0)
-else:
-    dp=[[0]*2 for i in range(n)]
-    dp[0][1]=number[0]
-    for i in range(1,n):
-        dp[i][0]=dp[i-1][1]
-        dp[i][1]=min(dp[i-1])+number[i]
-    print(min(dp[-1]))
+dp=[[0]*2 for i in range(n)]
+dp[0][1]=number[0]
+for i in range(1,n):
+    dp[i][0]=dp[i-1][1]
+    dp[i][1]=min(dp[i-1])+number[i]
+
+print(min(dp[-1]))
 
