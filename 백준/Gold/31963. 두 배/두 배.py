28e8306d918be7bbeeb133fac98fdf37b2276f66
patata22 <98090883+patata22@users.noreply.github.com>
2024-11-19 17:04:49 +0900

    [Gold IV] Title: 두 배, Time: 4276 ms, Memory: 2073448 KB, Score: 52 point -BaekjoonHub

--- 백준/Gold/31963. 두 배/두 배.py
+++ 백준/Gold/31963. 두 배/두 배.py
@@ -1,10 +1,20 @@
-n=int(input())
+def binary_search(left,right):
+    l=0
+    r=250001
+    while l+1<r:
+        mid=(l+r)//2
+        if left>right<<mid: l=mid
+        else: r=mid
+    return r
 
+
+n=int(input())
 number=list(map(int,input().split()))
 answer=0
 for i in range(1,n):
-    while number[i]<number[i-1]:
-        number[i]<<=1
-        answer+=1
+    if number[i-1]>number[i]:
+        need = binary_search(number[i-1],number[i])
+        number[i]<<=need
+        answer+=need
 
 print(answer)
