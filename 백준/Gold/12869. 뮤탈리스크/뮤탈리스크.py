27906ed024ce01ea57f31f56764de9a10808068c
patata22 <98090883+patata22@users.noreply.github.com>
2024-12-12 06:28:54 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 52 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.py
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.py
@@ -1,52 +1,12 @@
-from collections import deque
-
-def sol2():
-    x,y=map(int,input().split())
-    q=deque()
-    q.append((x,y))
-    visited=[[0]*61 for i in range(61)]
-    visited[x][y]=1
-    t=0
-    damage=((9,3),(3,9))
-    while q:
-        for _ in range(len(q)):
-            x,y=q.popleft()
-            if not x and not y: return t
-            for dx,dy in damage:
-                nx,ny=max(0,x-dx),max(0,y-dy)
-                if not visited[nx][ny]:
-                    visited[nx][ny]=1
-                    q.append((nx,ny))
-        t+=1
-
-def sol3():
-    x,y,z=map(int,input().split())
-    q=deque()
-    q.append((x,y,z))
-    visited=[[[0]*61 for i in range(61)] for i in range(61)]
-    visited[x][y][z]=1
-    t=0
-    damage=((1,3,9),(1,9,3),(3,1,9),(3,9,1),(9,1,3),(9,3,1))
-    while q:
-        for _ in range(len(q)):
-            x,y,z=q.popleft()
-            if not x and not y and not z: return t
-            for dx,dy,dz in damage:
-                nx,ny,nz=max(0,x-dx),max(0,y-dy),max(0,z-dz)
-                if not visited[nx][ny][nz]:
-                    visited[nx][ny][nz]=1
-                    q.append((nx,ny,nz))
-        t+=1
-    
-
+d=((1,3,9),(1,9,3),(3,1,9),(3,9,1),(9,1,3),(9,3,1))
+def s(x,y,z):
+    if x+y+z==0:return 0
+    if p[x][y][z]==61:
+        for a,b,c in d:
+            e,f,g=max(0,x-a),max(0,y-b),max(0,z-c)
+            p[x][y][z]=min(p[x][y][z],s(e,f,g)+1)
+    return p[x][y][z]
 n=int(input())
-if n==1:
-    hp=int(input())
-    answer=hp//9
-    if hp%9: answer+=1
-    print(answer)
-
-elif n==2:
-    print(sol2())
-else:
-    print(sol3())
+h=list(map(int,input().split()))+[0]*(3-n)
+p=[[[61]*61 for i in range(61)] for i in range(61)]
+print(s(*h))
