7ec44384c8198ea97345f64e0a54170aafd553b1
patata22 <98090883+patata22@users.noreply.github.com>
2024-12-21 20:18:16 +0900

    [Gold IV] Title: 텔레포트, Time: 144 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/16958. 텔레포트/텔레포트.py
+++ 백준/Gold/16958. 텔레포트/텔레포트.py
@@ -3,31 +3,30 @@ input=sys.stdin.readline
 
 n,t=map(int,input().split())
 special=[0]*n
+spe=[]
 city=[]
+spedist=[float('inf')]*n
 for i in range(n):
     s,x,y=map(int,input().split())
     special[i]=s
+    if s: spe.append((x,y))
     city.append((x,y))
 
-distance=[[float('inf')]*n for i in range(n)]
-
 for i in range(n):
-    distance[i][i]=0
-    x1,y1=city[i]
-    for j in range(i+1,n):
-        x2,y2=city[j]
-        dist=abs(x1-x2)+abs(y1-y2)
-        if special[i] and special[j]:
-            distance[i][j]=distance[j][i]=min(dist,t)
-        else: distance[i][j]=distance[j][i]=dist
-
-for k in range(n):
-    for i in range(n):
-        for j in range(i+1,n):
-            if distance[i][j]>distance[i][k]+distance[k][j]:
-                distance[i][j]=distance[i][k]+distance[k][j]
-                distance[j][i]=distance[i][j]
+    if special[i]:
+        spedist[i]=0
+    else:
+        x1,y1=city[i]
+        for x2,y2 in spe:
+            spedist[i] = min(spedist[i],abs(x1-x2)+abs(y1-y2))
 
 for _ in range(int(input())):
     a,b=map(int,input().split())
-    print(distance[a-1][b-1])
+    a-=1
+    b-=1
+    x1,y1=city[a]
+    x2,y2=city[b]
+    dist=abs(x1-x2)+abs(y1-y2)
+    if special[a] and special[b]:print(min(dist,t))
+    else:
+        print(min(abs(x1-x2)+abs(y1-y2),spedist[a]+spedist[b]+t))
