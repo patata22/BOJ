45dc087535786347a385704fd7bf5096b9ef3f96
patata22 <98090883+patata22@users.noreply.github.com>
2025-01-04 12:43:31 +0900

    [Gold V] Title: Birmingham, Time: 620 ms, Memory: 61268 KB -BaekjoonHub

---
+++ 백준/Gold/19231. Birmingham/Birmingham.py
@@ -0,0 +1,48 @@
+from collections import deque
+import sys
+input=sys.stdin.readline
+
+
+def calc(x,k):
+    l=0
+    r=10000
+    total=2*x
+    while l+1<r:
+        mid=(l+r)//2
+        if mid*(mid+1)*k>=total:r=mid
+        else: l=mid
+    return r
+
+def sol():
+    
+    n,m,q,k=map(int,input().split())
+
+    q=deque()
+    visited=[-1]*n
+    for x in map(int,input().split()):
+        q.append(x-1)
+        visited[x-1]=0
+    t=1
+    graph=[[] for i in range(n)]
+    for _ in range(m):
+        a,b=map(int,input().split())
+        graph[a-1].append(b-1)
+        graph[b-1].append(a-1)
+
+    while q:
+        for _ in range(len(q)):
+            now=q.popleft()
+            for to in graph[now]:
+                if visited[to]<0:
+                    visited[to]=t
+                    q.append(to)
+        t+=1
+
+    answer=[]
+    for x in visited:
+        if not x: answer.append(0)
+        else:answer.append(calc(x,k))
+    print(*answer)
+        
+
+sol()
