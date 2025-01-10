d9cb329e01aeee3b46c742137356bf0ab0ddf1fd
patata22 <98090883+patata22@users.noreply.github.com>
2025-01-10 17:16:42 +0900

    [Gold IV] Title: 리조트, Time: 44 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/13302. 리조트/리조트.py
+++ 백준/Gold/13302. 리조트/리조트.py
@@ -2,13 +2,13 @@ n,m=map(int,input().split())
 temp=[]
 if m: temp=set(map(int,input().split()))
 
+
 dp=[[float('inf')]*41 for i in range(n+1)]
 dp[0][0]=0
 for i in range(1,n+1):
     if i in temp:
         for j in range(41):
             dp[i][j]=min(dp[i][j],dp[i-1][j])
-        continue
     for j in range(41):
         if j>=3: dp[i][j-3]=min(dp[i][j-3],dp[i-1][j])
         dp[i][j]=min(dp[i][j],dp[i-1][j]+10000)
