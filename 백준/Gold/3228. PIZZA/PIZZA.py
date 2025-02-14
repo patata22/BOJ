900b6f02946c9da634ec12370fc728c8e3841cd9
patata22 <98090883+patata22@users.noreply.github.com>
2025-02-14 10:36:26 +0900

    [Gold V] Title: PIZZA, Time: 640 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/3228. PIZZA/PIZZA.py
+++ 백준/Gold/3228. PIZZA/PIZZA.py
@@ -10,28 +10,30 @@ def sol(depth,start,result):
       answer=max(answer,result)
       return
    for i in range(start,n):
+      x1,y1=restaurant[i]
       temp=0
       v=[]
-      x1,y1=restaurant[i]
-      for j in range(m):
-         x2,y2,p=point[j]
-         if not visited[j] and check(x1,y1,x2,y2):
+      for j in graph[i]:
+         if not visited[j]:
             visited[j]=1
             v.append(j)
-            temp+=p
+            temp+=point[j][2]
       sol(depth+1,i+1,result+temp)
       for x in v: visited[x]=0
 
 d,r=map(int,input().split())
-
 n=int(input())
 restaurant=[tuple(map(int,input().split())) for i in range(n)]
 m=int(input())
 point=[tuple(map(int,input().split())) for i in range(m)]
+graph=[[] for i in range(n)]
+for i in range(n):
+   x1,y1=restaurant[i]
+   for j in range(m):
+      x2,y2,p=point[j]
+      if check(x1,y1,x2,y2): graph[i].append(j)
 
 visited=[0]*m
-
 answer=0
-
 sol(0,0,0)
 print(answer)
