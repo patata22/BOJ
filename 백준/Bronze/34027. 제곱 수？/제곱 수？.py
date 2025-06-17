48b02499fddc93e22f8d8b8f78617be637162236
patata22 <98090883+patata22@users.noreply.github.com>
2025-06-17 20:39:56 +0900

    [Bronze III] Title: 제곱 수?, Time: 312 ms, Memory: 209428 KB -BaekjoonHub

--- 백준/Bronze/34027. 제곱 수？/제곱 수？.py
+++ 백준/Bronze/34027. 제곱 수？/제곱 수？.py
@@ -1,2 +1,1 @@
-n=set([i*i for i in range(1,32625)])
-for _ in range(int(input())): print(int(int(input()) in n))
+for _ in range(int(input())): print(int(int(input()) in [i*i for i in range(1,1<<15)]))
