aa2271698baafbb9630084ea9620be49a6638b79
patata22 <98090883+patata22@users.noreply.github.com>
2025-08-17 18:13:35 +0900

    [Gold III] Title: Ontongdaejeon, Time: 2644 ms, Memory: 56644 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/25115. Ontongdaejeon/Ontongdaejeon.py
+++ 백준/Gold/25115. Ontongdaejeon/Ontongdaejeon.py
@@ -1,33 +1,24 @@
 def sol():
     l=0
-    r=10**15
+    r=10**16
     while l+1<r:
         mid=(l+r)//2
         if check(mid): r=mid
         else: l=mid
-    return r
+    if r%10: return r//10+1
+    return r//10
 
-def check(now):
+def check(cash):
     point=0
-    for i in range(n):
-        if now+point>=reverse[i]: return True
-        price=number[i]
-        last=price%10
-        if last<=point:
-            point-=last
-            price-=last
-        if now<price: return False
-        now-=price
-        point+=price//10
+    for price in number:
+        if price>point+cash: return False
+        cashUse=min(cash,price)
+        pointUse=price-cashUse
+        cash-=cashUse
+        point-=pointUse
+        point+=cashUse//10
     return True
-        
 
 n=int(input())
-
-number=tuple(map(int,input().split()))
-
-reverse=[0]*n
-reverse[-1]=number[-1]
-for i in range(n-2,-1,-1):
-    reverse[i]=reverse[i+1]+number[i]
+number=tuple(map(lambda x: int(x)*10,input().split()))
 print(sol())
